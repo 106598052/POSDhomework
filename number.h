5f7d0069d4d9c0780ed3a8be333486da3f5b0d6f
106598052 <msps50807@gmail.com>
2017-10-26 14:28:45 +0800

    push number

---
+++ number.h
@@ -0,0 +1,18 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+
+#include <string>
+#include <sstream>
+#include "term.h"
+using std::string;
+
+class Number : public Term{
+public:
+  Number(double db){
+    std::ostringstream strs;
+    strs << db;
+    _symbol = strs.str();
+  }
+};
+#endif
